369303d445f23a2a2049fbd165a22fa36fe69d85
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2023-02-06 16:15:13 +0530

    lab1

---
+++ Lab1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lab 1");
+        }
+    }
+}
