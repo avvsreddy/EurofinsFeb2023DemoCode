cdcc89af2d38d7342bec8f71ebb8948c14224941
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2023-02-06 16:03:09 +0530

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        public string PersonName;
+        static void Main(string[] args)
+        {
+            string personName = "sdfsdfsd";
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
