fb951a61248e8eaae4e9727e938b49cc6e54c189
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2023-02-15 11:07:32 +0530

    ide

---
+++ ide-languageApp/Program.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+
+namespace ide_languageApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IDE ide = new IDE();
+            ide.Languages.Add(new LangCSharp());
+            ide.Languages.Add(new LangJava());
+            ide.Languages.Add(new LangC());
+
+            ide.WorkWithLanguages();
+        }
+    }
+
+
+    interface ILanguage
+    {
+        string GetName();
+        string GetUnit();
+        string GetParadigm();
+    }
+
+    class IDE
+    {
+        //public LangJava Java { get; set; }
+        //public LangCSharp CSharp { get; set; }
+        //public LangC C { get; set; }
+
+        public List<ILanguage> Languages { get; set; } = new List<ILanguage>();
+
+        public void WorkWithLanguages()
+        {
+            foreach (ILanguage language in Languages)
+            {
+                System.Console.WriteLine(language.GetName());
+                System.Console.WriteLine(language.GetUnit());
+                System.Console.WriteLine(language.GetParadigm());
+                System.Console.WriteLine("---------------");
+            }
+
+            //System.Console.WriteLine(CSharp.GetName());
+            //System.Console.WriteLine(CSharp.GetUnit());
+            //System.Console.WriteLine(CSharp.GetParadigm());
+            //System.Console.WriteLine("---------------");
+            //System.Console.WriteLine(C.GetName());
+            //System.Console.WriteLine(C.GetUnit());
+            //System.Console.WriteLine(C.GetParadigm());
+        }
+    }
+
+    class LangJava : ILanguage
+    {
+        public string GetName()
+        {
+            return "Java";
+        }
+        public string GetUnit()
+        {
+            return "Class";
+        }
+        public string GetParadigm()
+        {
+            return "Object Oriented";
+        }
+    }
+    class LangCSharp : ILanguage
+    {
+        public string GetName()
+        {
+            return "CSharp";
+        }
+        public string GetUnit()
+        {
+            return "Class";
+        }
+        public string GetParadigm()
+        {
+            return "Object Oriented";
+        }
+    }
+    class LangC : ILanguage
+    {
+        public string GetName()
+        {
+            return "C Language";
+        }
+        public string GetUnit()
+        {
+            return "Function";
+        }
+        public string GetParadigm()
+        {
+            return "Procedural Oriented";
+        }
+    }
+}
