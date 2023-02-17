a06b7f02989804443a1f545ce13dc6464325055f
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2023-02-17 13:02:24 +0530

    oo

--- CollectionsDemo3/Program.cs
+++ CollectionsDemo3/Program.cs
@@ -40,6 +40,37 @@ namespace CollectionsDemo3
             int x = queue.Dequeue();
             x = queue.Peek();
 
+            // Hash Set
+            HashSet<int> set = new HashSet<int>();
+            set.Add(1);
+            set.Add(2);
+            set.Add(2);
+            Console.WriteLine(set.Count);
+
+            // dictionary 
+            Dictionary<int, string> results = new Dictionary<int, string>();
+            results.Add(111, "Pass");
+            results.Add(222, "Fail");
+            //results.Add(222, "Pass");
+
+            // read
+
+            string res = results[222];
+
+
+            Dictionary<int, string> days = new Dictionary<int, string>();
+            days.Add(1, "Sunday");
+            days.Add(2, "Monday");
+            days.Add(3, "Tuesday");
+
+            days.Add(7, "Saturday");
+
+            Console.WriteLine(days[3]);
+
+            Dictionary<int, List<Dictionary<int, string>>> results1 = new Dictionary<int, List<Dictionary<int, string>>>();
+
+            days.ContainsKey(1);
+
         }
     }
 }
