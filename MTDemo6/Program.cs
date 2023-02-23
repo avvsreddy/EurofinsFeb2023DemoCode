1ecacedafb1dfe475fdbef07e91a45b1d5af8260
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2023-02-23 15:18:25 +0530

    mt

---
+++ MTDemo6/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace MTDemo6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BigData data = new BigData();
+            //data.Fill();
+            //data.Fill();
+            Parallel.Invoke(data.Fill, data.Fill);
+            System.Console.WriteLine(data.Count);
+
+        }
+    }
+
+    public class BigData
+    {
+        //private Stack<int> stack = new Stack<int>();
+        private ConcurrentStack<int> stack = new ConcurrentStack<int>();
+        //[MethodImpl(MethodImplOptions.Synchronized)]
+        public void Fill()
+        {
+            // Fill a million numbers
+            for (int i = 1; i <= 1000000; i++)
+            {
+                //lock (this)
+                //{
+                //Monitor.Enter(this);
+                //sddfsdf
+                stack.Push(i);
+                //sdfsdf
+                //Monitor.Exit(this);
+                //}
+
+            }
+        }
+        public long Count
+        {
+            get { return stack.Count; }
+        }
+    }
+}
---
+++ MTDemo7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MTDemo7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            M1();
+        }
+
+        static void M1()
+        {
+            int pcount = Environment.ProcessorCount;
+            ParallelOptions opts = new ParallelOptions();
+            opts.MaxDegreeOfParallelism = pcount / 2;
+
+            HashSet<int> list = new HashSet<int>();
+            Object obj = new object();
+            Parallel.For(1, 100, opts, i =>
+            {
+                lock (obj)
+                {
+                    list.Add(Thread.CurrentThread.ManagedThreadId);
+                }
+                // sdfsdfd
+                // sdfsdfsdf
+            });
+            Console.WriteLine(list.Count);
+        }
+    }
+}
